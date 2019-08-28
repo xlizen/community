c91db29bcc7c038727cde68cda24c46fb0ca2e22
xlizen <m13194241830@163.com>
2019-08-28 21:14:07 +0800

    add index and bootstrap

---
+++ src/main/java/com/xlizen/community/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.xlizen.community.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+
+    @GetMapping("/")
+    private String index(){
+        return "index";
+    }
+}
