249445bdb370395e09aca29c150ef787fc144332
xlizen <m13194241830@163.com>
2019-08-31 17:54:45 +0800

    add h2 database

--- src/main/java/com/xlizen/community/controller/AuthorizeController.java
+++ src/main/java/com/xlizen/community/controller/AuthorizeController.java
@@ -3,12 +3,17 @@ package com.xlizen.community.controller;
 
 import com.xlizen.community.dto.AccessTokenDTO;
 import com.xlizen.community.dto.GithubUserDTO;
+import com.xlizen.community.mappper.UserMapper;
+import com.xlizen.community.model.User;
 import com.xlizen.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -25,9 +30,13 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO tokenDTO = new AccessTokenDTO();
         tokenDTO.setClient_id(clientId);
         tokenDTO.setCode(code);
@@ -35,8 +44,22 @@ public class AuthorizeController {
         tokenDTO.setRedirect_uri(redirectUri);
         tokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(tokenDTO);
-        GithubUserDTO githubUserDTO = githubProvider.getGithubUserDTO(accessToken);
-        System.out.println(githubUserDTO.getLogin());
-        return "index";
+        GithubUserDTO githubUser = githubProvider.getGithubUserDTO(accessToken);
+        if (githubUser != null) {
+            //登录成功，写cookie和session
+            User user = new User();
+            user.setName(githubUser.getLogin());
+            user.setToken(UUID.randomUUID().toString());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        } else {
+            //重新登陆
+            return "redirect:/";
+        }
+
     }
 }
---
+++ src/main/java/com/xlizen/community/mappper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.xlizen.community.mappper;
+
+import com.xlizen.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+    @Insert("insert into user(account_id,name,token,gmt_create,gmt_modified) " +
+            "values(#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified}) ")
+    void insert(User user);
+}
